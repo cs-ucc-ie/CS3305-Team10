f1284d530690eaff6317ca1c195f99e4e52bfc3e
Filip K. <102953124+respawn999@users.noreply.github.com>
2024-01-24 16:57:01 +0000

    updated prototype, outputs image used for validation

--- detector.py
+++ detector.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from collections import Counter
+from PIL import Image, ImageDraw
 import face_recognition
 import pickle
 
@@ -48,12 +49,19 @@ def recognize_faces(
     input_face_locations = face_recognition.face_locations(input_image, model=model)
     input_face_encodings = face_recognition.face_encodings(input_image, input_face_locations)
     
+    pillow_image = Image.fromarray(input_image)
+    draw = ImageDraw.Draw(pillow_image)
+    
+    
     
     for bounding_box, unknown_encoding in zip(input_face_locations, input_face_encodings):
         name = _recognize_face(unknown_encoding, loaded_encodings)
         if not name:
             name = "Unknown"
-        print(name, bounding_box)
+        _display_face(draw, bounding_box, name)
+        
+        del draw
+        pillow_image.show()
         
         
 def _recognize_face(unknown_encoding, loaded_encodings):
@@ -67,4 +75,25 @@ def _recognize_face(unknown_encoding, loaded_encodings):
     if votes:
         return votes.most_common(1)[0][0]
     
-recognize_faces("unknown.jpg")
+    
+BOUNDING_BOX_COLOR = "blue"
+TEXT_COLOR = "white"
+    
+def _display_face(draw, bounding_box, name):
+    top, right, bottom, left = bounding_box
+    draw.rectangle(((left, top), (right, bottom)), outline = BOUNDING_BOX_COLOR)
+    text_left, text_top, text_right, text_bottom = draw.textbbox((left, bottom), name)
+    
+    draw.rectangle(((text_left, text_top), (text_right,text_bottom)), fill="blue", outline="blue")
+    draw.text((text_left, text_top), name, fill = "white")
+    
+    
+def validate(model: str = "cnn"):
+    for filepath in Path("validation").rglob("*"):
+        if filepath.is_file():
+            recognize_faces(
+                image_location = str(filepath.absolute()), model=model)
+            
+
+    
+validate()
